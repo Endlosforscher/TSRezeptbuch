c3c30f92de6995bb2701933bb53e04b057d5716e
Endlosforscher <luigi.sabbetti@gmail.com>
2024-05-08 11:57:39 +0200

    interfaces first commit

---
+++ docs/09_Interfaces.ts
@@ -0,0 +1,8 @@
+/* 
+In TypeScript, interfaces are a way to define the shape 
+or structure of an object. They describe the properties and 
+methods that an object should have, but they don't provide 
+any implementation. Interfaces allow you to enforce a contract
+between different parts of your code, ensuring consistency 
+and interoperability. 
+*/
