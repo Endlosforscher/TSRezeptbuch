b11b7e24591b048beca9ed6087515d7ec8316c76
Endlosforscher <luigi.sabbetti@gmail.com>
2024-05-12 18:52:43 +0200

    Interfaces documentation

--- docs/09_Interfaces.ts
+++ docs/09_Interfaces.ts
@@ -6,3 +6,58 @@ any implementation. Interfaces allow you to enforce a contract
 between different parts of your code, ensuring consistency 
 and interoperability. 
 */
+
+// Create a basic interface
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    greet(): void;
+}
+
+// Implements
+class Student implements Person {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public age: number
+    ) {}
+    greet(): void {
+        console.log(`Name: ${this.firstName} and ${this.lastName}`)
+    }
+}
+
+// Using interface as type annotations
+const printPerson = (person: Person):void => {
+    console.log(`Firstname: ${person.firstName} and lastname: ${person.lastName}`)
+    person.greet()
+} 
+
+const john: Person = {
+    firstName: 'John',
+    lastName : 'Doe',
+    age: 20,
+    greet() {
+        console.log(`Greeting ${this.firstName}`)
+    }
+} 
+
+printPerson(john)
+
+// Using interfaces as function annotations
+interface MathFunction {
+    (z:number, j:number) : number
+}
+
+const sumNumbers: MathFunction = (z:number, j:number): number => {
+    return z + j
+}
+
+const multiplyNumbers: MathFunction = (z:number, j:number): number => {
+    return z + j
+}
+
+const subNumbers: MathFunction = (z:number, j: number) : number => {
+    return z - j
+}
+
